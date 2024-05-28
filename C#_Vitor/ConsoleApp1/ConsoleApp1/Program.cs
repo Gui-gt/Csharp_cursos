899e2ea59c3a4edc011eca7e3327921013015549
Gui-gt <30755guilherme@gmail.com>
2024-05-28 20:27:21 -0300

    ttt

--- C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,19 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string nome;
-            Console.WriteLine("Qual é o seu nome?");
-            nome = Console.ReadLine();
-            Console.WriteLine("Seu nome é {0}?", nome);
-            if(nome == "Guilherme")
-            {
-                Console.WriteLine("Prazer em te conhecer então");
-            }
-            else if(nome != "Guilherme")
-            {
-                Console.WriteLine("Desculpe o incômodo então");
-            }
-            Console.ReadLine();
-        }
-    }
+
+            //int = 8
+            //float = 8.8f
+            //string = "texto"
+            //char = caracter = 'G'
+
+            //const = valor que jamais muda
+            //var = jeito preguiçoso de atribuir valores
+
+            // C# pode fazer qualquer coisa, Jogos, aplicações desktop, sistemas web e muito mais.
+            // .NET ou dotNet é uma plataforma que contém todos os módulos da linguagem, pois se não sobrecarregaria o código
+            //using ( "usando uma módulo ou biblioteca C#, para certo fim")
+            //existem várias bibliotecas com várias finalidades
+            //.NET só é para Windows
+
+            int numeroQualquer = 20 + 20 - 80;
+            int numeroDiv = 6 / 2;
+            float numeroX = 5.0f / 2;
+
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroDiv);
+            Console.WriteLine(numeroX);
+
+
+            Console.ReadLine();               
+        }                                     
+    }                                         
 }
