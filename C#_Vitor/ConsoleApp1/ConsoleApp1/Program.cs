cda957622284f3d54f43c75460ca70a1ae8e90af
Gui-gt <30755guilherme@gmail.com>
2024-05-28 20:16:03 -0300

    att

---
+++ C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Qual é o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é {0}?", nome);
+            if(nome == "Guilherme")
+            {
+                Console.WriteLine("Prazer em te conhecer então");
+            }
+            else if(nome != "Guilherme")
+            {
+                Console.WriteLine("Desculpe o incômodo então");
+            }
+            Console.ReadLine();
+        }
+    }
+}
