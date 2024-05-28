f3465154e187e501d1696e07c56dbff255e6308c
Gui-gt <30755guilherme@gmail.com>
2024-05-28 19:13:47 -0300

    ttt

--- C#_Vitor/Console/Console/Program.cs
+++ C#_Vitor/Console/Console/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Console
 {
@@ -6,7 +10,7 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word");
+            //lll
         }
     }
 }
