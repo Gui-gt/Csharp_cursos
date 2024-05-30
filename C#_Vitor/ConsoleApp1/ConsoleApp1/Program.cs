126008355411db591f04ed00d1e24e03fe24648a
Gui-gt <30755guilherme@gmail.com>
2024-05-30 12:40:00 -0300

    att

--- C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -28,185 +28,226 @@ namespace ConsoleApp1
             //using ( "usando uma módulo ou biblioteca C#, para certo fim")
             //existem várias bibliotecas com várias finalidades
             //.NET só é para Windows
+            using System;
 
-            /*int numeroQualquer = 20 + 20 - 80;
-            int numeroDiv = 6 / 2;
-            float numeroX = 5.0f / 2;
-
-
-            Console.WriteLine(numeroQualquer);
-            Console.WriteLine(numeroDiv);
-            Console.WriteLine(numeroX);*/
-
-            //Ordem de precedência
-            /*Primeiro * (MULTIPLICAÇÃO) e / (DIVISÃO)
-            Depois + e -, Porém se ouver (), a ordem mudo e começa por eles.
-            Exemplo:
-
-            n = 2 + 2 * 10 = 22 sem parentêses
-            n = (2 + 2) * 10 = 40 com () */
-
-            //Operadores Aritméticos
-
-            //> < = == >= <= !=
-
-            //Operadores Lógicos
-
-            //(AND e OR) && ||
-
-            /*int n = 2 + 2 * 10;
-              int num = (2 + 2) * 10;
-
-            Console.WriteLine(n);
-            Console.WriteLine(num);*/
-
-            //Condicionais em C#
-
-            /* Console.WriteLine("Digite sua idade");
-             int idade = int.Parse(Console.ReadLine());    
-
-             if(idade >= 0 && idade <= 11)
-             {
-                 Console.WriteLine("Voçê é uma criança");
-             }else if(idade  >= 11 && idade <= 18)
-             {
-                 Console.WriteLine("Voçê é um adolescente");
-             }else if(idade >= 19 && idade <= 60)
-             {
-                 Console.WriteLine("Voçê é um adulto");
-             }else
-             {
-                 Console.WriteLine("Voçê é um idoso");
-             }*/
-
-
-            //Funçôes em C#
-
-            // Uma função é um método que pode ser chamado no método principal (MAIN):
-            //Por exemplo:
-
-            /*static void Main()
+namespace Program
+    {
+        public class Program
+        {
+            enum Opcao { Criar = 1, Deletar, Editar, Listar, Atualizar };
+            static void Main()
             {
-                Guilherme(); // Aqui será impresso o que está dentro da função Guilherme 
+                Console.WriteLine("Escolha uma das opções abaixo:");
+                Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listar\n5-Atualizar");
+                int index = int.Parse(Console.ReadLine());
+                Opcao opcaoSelecionada = (Opcao)index;
+
+                Console.WriteLine(opcaoSelecionada);
+
+                switch (opcaoSelecionada)
+                {
+                    case Opcao.Criar:
+                        break;
+
+                    case Opcao.Deletar:
+                        break;
+
+                    case Opcao.Editar:
+                        break;
+
+                    case Opcao.Listar:
+                        break;
+
+                    case Opcao.Atualizar:
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Opção inválida");
+
+                        break;
+                }
             }
+        }
+    }
+    /*int numeroQualquer = 20 + 20 - 80;
+    int numeroDiv = 6 / 2;
+    float numeroX = 5.0f / 2;
+
+
+    Console.WriteLine(numeroQualquer);
+    Console.WriteLine(numeroDiv);
+    Console.WriteLine(numeroX);*/
+
+    //Ordem de precedência
+    /*Primeiro * (MULTIPLICAÇÃO) e / (DIVISÃO)
+    Depois + e -, Porém se ouver (), a ordem mudo e começa por eles.
+    Exemplo:
+
+    n = 2 + 2 * 10 = 22 sem parentêses
+    n = (2 + 2) * 10 = 40 com () */
+
+    //Operadores Aritméticos
+
+    //> < = == >= <= !=
+
+    //Operadores Lógicos
+
+    //(AND e OR) && ||
+
+    /*int n = 2 + 2 * 10;
+      int num = (2 + 2) * 10;
+
+    Console.WriteLine(n);
+    Console.WriteLine(num);*/
+
+    //Condicionais em C#
+
+    /* Console.WriteLine("Digite sua idade");
+     int idade = int.Parse(Console.ReadLine());    
+
+     if(idade >= 0 && idade <= 11)
+     {
+         Console.WriteLine("Voçê é uma criança");
+     }else if(idade  >= 11 && idade <= 18)
+     {
+         Console.WriteLine("Voçê é um adolescente");
+     }else if(idade >= 19 && idade <= 60)
+     {
+         Console.WriteLine("Voçê é um adulto");
+     }else
+     {
+         Console.WriteLine("Voçê é um idoso");
+     }*/
+
+
+    //Funçôes em C#
+
+    // Uma função é um método que pode ser chamado no método principal (MAIN):
+    //Por exemplo:
+
+    /*static void Main()
+    {
+        Guilherme(); // Aqui será impresso o que está dentro da função Guilherme 
+    }
 
 
 
-            static void Guilherme ()
-            {
-                Console.WriteLine("Seja Bem vindo");
-                Console.WriteLine("Estou aprendendo c#");
-                Console.WriteLine("que curso show de bola");
-            }*/
+    static void Guilherme ()
+    {
+        Console.WriteLine("Seja Bem vindo");
+        Console.WriteLine("Estou aprendendo c#");
+        Console.WriteLine("que curso show de bola");
+    }*/
 
-            // GerarProduto(-20, "Curso de Node JS");
-
-
-            //string nome =Console.ReadLine(); 
-            //Console.WriteLine(nome);
-            //Console.ReadLine();
-
-            // int soma1 = Somar(1 , 2, 3);
-
-            //Console.WriteLine(soma1);
+    // GerarProduto(-20, "Curso de Node JS");
 
 
-            // Arrays
+    //string nome =Console.ReadLine(); 
+    //Console.WriteLine(nome);
+    //Console.ReadLine();
 
-            /*string produto1 = "Minecraft";
-            string produto2 = "Call of Duty";
-            string produto3 = "Street Fitgher";      //Para guardar muitos itens isso seria um problema, o mais ideal seria usando um array;
-            string produto4 = "Dungeos";
-            string produto5 = "Need for speed";
+    // int soma1 = Somar(1 , 2, 3);
 
-            string[] Produtos = new string[5]
-
-                {   "Minecraft",
-                    "Need For Speed",                   //Assim fica bem melhor e legível
-                    "Fifa", 
-                    "Dungeos",
-                    "Street Fitgher"
-                };
-
-            Produtos[3] = "Minecraft Dungeos"; // Forma de alterar um item;
-
-            Console.WriteLine(Produtos[3]);
-
-            int[] valores = { 11,22,33,44,55,66 };      // mais simples e organizado;*/
+    //Console.WriteLine(soma1);
 
 
-            //Swith Case
+    // Arrays
 
-            /* string cor = "preto";
+    /*string produto1 = "Minecraft";
+    string produto2 = "Call of Duty";
+    string produto3 = "Street Fitgher";      //Para guardar muitos itens isso seria um problema, o mais ideal seria usando um array;
+    string produto4 = "Dungeos";
+    string produto5 = "Need for speed";
 
-             switch (cor)
-             {
-                     case "Vermelho":
-                     Console.WriteLine("Sua cor preferida é vermelho!");
-                     break;
+    string[] Produtos = new string[5]
 
-                 case "Amarelo":
-                     Console.WriteLine("Sua cor preferida é Amarelo!");
-                     break;
-                                                                                 // muito útil em casos que exigem muitos cases (casos);
-                 case "Verde":
-                     Console.WriteLine("Sua cor preferida é Verde!");
-                     break;
+        {   "Minecraft",
+            "Need For Speed",                   //Assim fica bem melhor e legível
+            "Fifa", 
+            "Dungeos",
+            "Street Fitgher"
+        };
 
-                 case "Azul":
-                     Console.WriteLine("Sua cor preferida é Azul!");
-                     break;
+    Produtos[3] = "Minecraft Dungeos"; // Forma de alterar um item;
 
-                 default:
-                     Console.WriteLine("Droga, não sei qual cor que é...");
-                     break;
-             }*/
+    Console.WriteLine(Produtos[3]);
+
+    int[] valores = { 11,22,33,44,55,66 };      // mais simples e organizado;*/
 
 
-            // Enums
+    //Swith Case
 
-            // Um Enum é como um irmão gemêo do var, ele guarda valores ou itens pequenos;
+    /* string cor = "preto";
+
+     switch (cor)
+     {
+             case "Vermelho":
+             Console.WriteLine("Sua cor preferida é vermelho!");
+             break;
+
+         case "Amarelo":
+             Console.WriteLine("Sua cor preferida é Amarelo!");
+             break;
+                                                                         // muito útil em casos que exigem muitos cases (casos);
+         case "Verde":
+             Console.WriteLine("Sua cor preferida é Verde!");
+             break;
+
+         case "Azul":
+             Console.WriteLine("Sua cor preferida é Azul!");
+             break;
+
+         default:
+             Console.WriteLine("Droga, não sei qual cor que é...");
+             break;
+     }*/
 
 
-            /*Cor CorFavoritaGiovana = Cor.Verde;
-            Cor CorFavoritaGuilherme = Cor.Roxo;
+    // Enums
 
-            Console.WriteLine((Cor)2);
-            Console.WriteLine((int)CorFavoritaGuilherme);
-            Console.WriteLine((int)CorFavoritaGiovana);*/
+    // Um Enum é como um irmão gemêo do var, ele guarda valores ou itens pequenos;
 
-            Console.WriteLine("Seleciona uma das opções abaixo: ");
-            Console.WriteLine(" 1-Criar\n 2-Deletar\n 3-Editar\n 4-Listar\n 5-Atualizar");
 
-            int index =int.Parse(Console.ReadLine()); 
-            Opcao opcaoSelecionada = (Opcao)index;
+    /*Cor CorFavoritaGiovana = Cor.Verde;
+    Cor CorFavoritaGuilherme = Cor.Roxo;
 
-            switch(opcaoSelecionada)
-            {
-                case Opcao.Criar:
-                    Console.WriteLine("Voçê quer criar");
-                    break;
+    Console.WriteLine((Cor)2);
+    Console.WriteLine((int)CorFavoritaGuilherme);
+    Console.WriteLine((int)CorFavoritaGiovana);*/
 
-                case Opcao.Deletar:
-                    Console.WriteLine("Delete tudo");
-                    break;
+    /*Console.WriteLine("Seleciona uma das opções abaixo: ");
+    Console.WriteLine(" 1-Criar\n 2-Deletar\n 3-Editar\n 4-Listar\n 5-Atualizar");
 
-                case Opcao.Editar:
-                    Console.WriteLine("Editando algo");
-                    break;
-                case Opcao.Listar:
-                    Console.WriteLine("Liste alguma lista");
-                    break;
-                case Opcao.Atualizar:
-                    Console.WriteLine("Atualizando o sistema");
-                    break;
-            }
+    int index =int.Parse(Console.ReadLine()); 
+    Opcao opcaoSelecionada = (Opcao)index;
 
-            Console.WriteLine(opcaoSelecionada.ToString());
-            
-            
+    switch(opcaoSelecionada)
+    {
+        case Opcao.Criar:
+            Console.WriteLine("Voçê quer criar");
+            break;
 
-            Console.ReadLine();
+        case Opcao.Deletar:
+            Console.WriteLine("Delete tudo");
+            break;
+
+        case Opcao.Editar:
+            Console.WriteLine("Editando algo");
+            break;
+        case Opcao.Listar:
+            Console.WriteLine("Liste alguma lista");
+            break;
+        case Opcao.Atualizar:
+            Console.WriteLine("Atualizando o sistema");
+            break;
+    }
+
+    Console.WriteLine(opcaoSelecionada.ToString());*/
+
+
+
+    Console.ReadLine();
         }   
         
         static void GerarProduto(int preco, string nome)
