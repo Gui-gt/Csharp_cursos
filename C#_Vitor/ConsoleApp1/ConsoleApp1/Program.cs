96570add2d927e45eeba9eafa88c63c228a68984
Gui-gt <30755guilherme@gmail.com>
2024-06-22 18:22:18 -0300

    att

--- C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#_Vitor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -477,71 +477,19 @@ namespace ConsoleApp1
 
            codificador.Serialize(stream, fruta);
 
-            /* List<string> listadoarquivo = (List<String>)codificador.Deserialize(stream);
-             Produtu prod = (Produtu)codificador.Deserialize(stream);
+           /* List<string> listadoarquivo = (List<String>)codificador.Deserialize(stream);
+            Produtu prod = (Produtu)codificador.Deserialize(stream);
+
+            
+            Console.WriteLine(prod.nome);
 
 
-             Console.WriteLine(prod.nome);
-
-
-             stream.Close(); */
-
-
-            using System;
-            using System.Collections.Generic;
-            using System.Linq;
-            using System.Text;
-            using System.Threading.Tasks;
-            using System.Runtime.Serialization.Formatters.Binary;
-            using System.IO;
-
-namespace ConsoleApp2
-    {
-        internal class Program
-
-
-        {
-
-
-            [System.Serializable]
-            struct Produto
-            {
-                public string nome;
-                public int preco;
-
-
-                public Produto(string nome, int preco)
-                {
-                    this.nome = nome;
-                    this.preco = preco;
-                }
+            stream.Close(); */
 
 
 
-            }
 
-            static void Main(string[] args)
-            {
-
-                Produto fruta = new Produto("Banana", 20);
-
-                string nome = "Kakaroto";
-
-                FileStream stream = new FileStream("arquivoZZ", FileMode.OpenOrCreate);
-                BinaryFormatter coder = new BinaryFormatter();
-
-                coder.Serialize(stream, nome);
-                coder.Serialize(stream, fruta);
-
-
-                Console.ReadLine();
-
-            }
-        }
-    }
-
-
-    Console.ReadLine();
+            Console.ReadLine();
            
 
            
